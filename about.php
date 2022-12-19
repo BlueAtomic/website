469b28f6933c1de4a501ef34e962caf3e88551fb
MiataBoy <MiataBoyMX@gmail.com>
2022-12-19 21:26:24 +0100

    add team

--- about.php
+++ about.php
@@ -6,23 +6,86 @@
         <title>About BlackFur Studios</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/team.css">
         <script src="https://kit.fontawesome.com/d1393c407a.js" crossorigin="anonymous"></script>
     </head>
 
-    <?php include 'assets/scripts/cachehandler.php';?>
+    <?php include 'cachehandler.php';?>
 
     <body>
-        <div>
+        <div class="text-center">
             <h1>About Us</h1>
             <p>We are a dedicated team of college students, with a dream to serve people with fun, minimalistic games whether on roblox, terminal or other means. We want to provide people with useful products that adhere to the FOSS initiative, allowing everyone to see the code that we apply, as well as allowing them to see that our products are not malicious. Additionally, we always love to receive contributions to our products by our users in our repositories.</p>
-            <!-- <img src="<?php getAv(1052129761285132419) ?>" alt="" height="500px">
-            <p> <?php getUser("zen", true) ?> </p> -->
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="<?php echo getUser("zen", false) ?>" alt="Card image cap">
-                <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <hr>
+            <h2>Our team</h2>
+
+            <section class="text-center py-5 justify-content-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("zen", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Zenith avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("zen", true) ?></h4>
+                                    <p class="text-muted">Owner</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("storm", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Stormsnuitje avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("storm", true) ?></h4>
+                                    <p class="text-muted">Owner</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("sith", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Sith avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("sith", true) ?></h4>
+                                    <p class="text-muted">Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("dino", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Dino avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("dino", true) ?></h4>
+                                    <p class="text-muted">avatarless Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("ndua", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Ndua avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("ndua", true) ?></h4>
+                                    <p class="text-muted">avatarless Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("yil", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Yil avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("yil", true) ?></h4>
+                                    <p class="text-muted">avatarless Developer</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-            </div>
+            </section>
+
         </div>
     </body>
 </html>
