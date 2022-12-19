ebb5d16a9c3be3242232d38abf137011cbd11e39
MiataBoy <MiataBoyMX@gmail.com>
2022-12-19 18:35:17 +0100

    Actually cache discord links

--- test.php
+++ test.php
@@ -1,10 +1,23 @@
 <?php
 $getDate = date("d");
 
-function loadJson() {
+include 'assets/php/users.php';
+
+function dayNr() {
     $json = file_get_contents('cache.json');
     $decoded = json_decode($json);
     return $decoded->day;
+}
+
+function getUser(string $user, bool $name) : string {
+    $json = file_get_contents('cache.json');
+    $decoding = json_decode($json);
+
+    if ($name) {
+        return $decoding->team->$user->name;
+    } else {
+        return $decoding->team->$user->avatar;
+    }
 }
 
 echo "<br>$getDate <- current date | saved date -> ";
@@ -23,7 +36,21 @@ try {
         $decoded = json_decode($json, true);
         $decoded = ["day" => $getDate];
 
-        $json = json_encode($decoded);
+        $array = Array (
+            "day" => $getDate,
+            "team" => Array (
+                "zen" => Array (
+                    "name" => getName(682609654504882186),
+                    "avatar" => getAv(682609654504882186)
+                ),
+                "storm" => Array (
+                    "name" => getName(1052129761285132419),
+                    "avatar" => getAv(1052129761285132419)
+                )
+            )
+        );
+
+        $json = json_encode($array);
         file_put_contents("cache.json", json_encode([]));
         file_put_contents('cache.json', $json);
 
