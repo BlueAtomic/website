954b2f9419eac438e617626e406f2189afe8f4bb
Joshua Slui <miataboymx@gmail.com>
2023-03-23 13:36:38 +0100

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
     <head>
-        <title>About BlackFur Studios</title>
+        <title>About Blue Atomic</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/css/main.css">
         <link rel="stylesheet" href="assets/css/team.css">
@@ -28,16 +28,6 @@
                     <div class="row">
                         <div class="col-md-6 col-lg-3">
                             <div class="img-block mb-5">
-                                <img src="<?php echo getUser("zen", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Zenith avatar">
-                                <div class="content mt-2">
-                                    <h4><?php echo getUser("zen", true) ?></h4>
-                                    <p class="text-muted">Owner</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 col-lg-3">
-                            <div class="img-block mb-5">
                                 <img src="<?php echo getUser("storm", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Stormsnuitje avatar">
                                 <div class="content mt-2">
                                     <h4><?php echo getUser("storm", true) ?></h4>
@@ -45,7 +35,27 @@
                                 </div>
                             </div>
                         </div>
+                        
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("duck", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Sith avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("duck", true) ?></h4>
+                                    <p class="text-muted">Developer</p>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="col-md-6 col-lg-3">
+                            <div class="img-block mb-5">
+                                <img src="<?php echo getUser("zen", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Zenith avatar">
+                                <div class="content mt-2">
+                                    <h4><?php echo getUser("zen", true) ?></h4>
+                                    <p class="text-muted">Owner</p>
+                                </div>
+                            </div>
+                        </div>
+                        
                         <div class="col-md-6 col-lg-3">
                             <div class="img-block mb-5">
                                 <img src="<?php echo getUser("sith", false) ?>" class="img-fluid  img-thumbnail rounded-circle" alt="Sith avatar">
