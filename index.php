ad5dbae229483520c0a4a9bd2377f18932b1df5e
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-19 13:00:32 +0200

    Add index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <title>Wolfsvale | Home</title>
+    <link rel="stylesheet" href="src/assets/styling/home.css">
+    <meta name="viewport" content="width=device-width">
+  </head>
+
+  <body>
+  <div class="flex-container">
+    <div>
+      <h1> <span class="text-highlight">Wolfsvale</span> Open-source software solutions</h1>
+      <h2 class="subtitle">Software for all markets and all your needs. <u>by students, for everyone</u>.</h2>
+    <div class="footer">
+      <a href="about.php"><button class="btn btn-primary">About & Products</button></a>
+      <a href="support.php"><button class="btn btn-primary">Get support</button></a>
+      <b class="copyright">© 2023 - <?php echo date("Y"); ?></b>
+    </div>
+
+    </div>
+      <img src="src/assets/images/AtomicLogo.png" alt="yay">
+    </div>
+  </body>
+</html>
