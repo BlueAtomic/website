b638ae2ff797d55ddf1ae98709d26c726a298110
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-22 13:40:46 +0200

    Somewhat fix formatting

--- index.php
+++ index.php
@@ -8,18 +8,16 @@
   </head>
 
   <body>
-  <div class="flex-container">
-    <div>
+    <div class="flex-container">
+      <div>
       <h1> <span class="text-highlight">Wolfsvale</span> Open-source software solutions</h1>
-      <h2 class="subtitle">Software for all markets and all your needs. <u>by students, for everyone</u>.</h2>
-    <div class="footer">
-      <a href="about.php"><button class="btn btn-primary">About & Products</button></a>
-      <a href="support.php"><button class="btn btn-primary">Get support</button></a>
-      <b class="copyright">© 2023 - <?php echo date("Y"); ?></b>
+      <h2 class="subtitle">Software for all markets and needs. <u>by students, for everyone</u>.</h2>
+      <div class="footer">
+        <a href="about.php"><button class="btn btn-primary">About & Products</button></a>
+        <a href="support.php"><button class="btn btn-primary">Get support</button></a>
+        <b class="copyright">© 2023 - <?php echo date("Y"); ?></b>
+      </div>
     </div>
-
-    <div id="logo">
-      <img src="src/assets/images/AtomicLogo.png" alt="yay" class="disablePhoneDisplay">
-    </div>
+    <img src="src/assets/images/AtomicLogo.png" alt="yay" class="disablePhoneDisplay ml-50">
   </body>
 </html>
