2c06e71ee7a2698b17319ca4be835552108ef6e3
Joshua Slui <joshuaslui0203@gmail.com>
2023-06-23 15:06:43 +0200

    Make footer a component

---
+++ src/assets/components/footer.php
@@ -0,0 +1,3 @@
+<footer style="margin-top: 10px">
+    <p>© 2023 - <?php echo date("Y") ?> by Wolfsvale Software<br>Contact: <a href="mailto:miataboymx@gmail.com" style="text-decoration: none;color:var(--white)">miataboymx@gmail.com</a> | KVK: Pending</p>
+</footer>
