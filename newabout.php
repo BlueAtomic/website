dd9718b5bc53efb7acf92a377a01de371872b9ae
Joshua Slui <joshua@intermixmanagement.nl>
2023-09-01 15:14:19 +0200

    initialization of new files

---
+++ newabout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <title>About Wolfsvale Software</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <meta name="author" content="Wolfsvale Software">
+        <meta name="description" content="Our mission at Wolfsvale is to provide the market with more FOSS products. We want to ensure that everyone gets a chance, no matter the money.">
+    </head>
+
+    <body>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sed aliquam risus. Pellentesque ante mi, scelerisque eget luctus et, hendrerit a felis. Integer semper fringilla erat ac hendrerit. Pellentesque ultricies magna dolor, eget maximus ex sodales at. Donec ac cursus dui, vitae fringilla tellus. Ut at pharetra mauris, pharetra ullamcorper turpis. Praesent arcu est, finibus at odio eget, molestie efficitur sem. Phasellus in libero tincidunt, mollis dui sed, euismod mauris. Mauris nunc libero, suscipit sed velit quis, tincidunt posuere nulla. Etiam pulvinar massa in leo mollis, non fringilla magna faucibus. Maecenas sit amet mi in sapien congue lobortis. Vivamus sit amet hendrerit nibh. Quisque pellentesque nulla et ex feugiat, ut posuere ipsum aliquam. Sed volutpat leo et turpis faucibus, bibendum pharetra risus convallis. Nulla quam neque, finibus vitae blandit vel, maximus vel justo. Pellentesque bibendum, est ut sagittis maximus, risus nisl lacinia nibh, nec luctus lectus massa nec risus.
+
+    Quisque augue lacus, aliquam ut eros ac, accumsan tempor sem. Ut nec gravida lacus. Nulla eu magna massa. Praesent facilisis nibh nunc, efficitur iaculis dolor consectetur ac. Mauris vitae odio at nunc hendrerit dictum. Integer et faucibus quam. Vivamus tortor lectus, pharetra et tellus vitae, consectetur tempor turpis. Cras in convallis ante, a porta magna. Vestibulum quis nulla quis erat mollis feugiat quis eu ex. Pellentesque enim mauris, rhoncus accumsan cursus ac, vestibulum a justo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus faucibus pharetra laoreet. Maecenas semper elementum nulla id condimentum. Donec elit risus, volutpat a dignissim lacinia, pellentesque eu lorem. Quisque sollicitudin justo vitae est luctus dictum.
+
+    Nam cursus nibh tempor libero laoreet, quis porttitor diam feugiat. Nullam iaculis mi eget gravida volutpat. Curabitur sit amet semper dui. Donec ultricies pretium urna, cursus dignissim est pulvinar id. Morbi ultricies quis massa at congue. Pellentesque placerat ante odio, nec dictum felis aliquet fringilla. Aenean sit amet mollis tortor. Donec facilisis, ante ultrices tincidunt mollis, urna arcu commodo eros, non imperdiet felis ligula a purus. Mauris tincidunt nulla non enim scelerisque, in tristique mi sagittis. Duis felis orci, mattis at magna quis, consectetur efficitur metus. Praesent nec lorem felis.
+
+    Etiam varius justo id lorem rutrum, eget rhoncus ex efficitur. Suspendisse potenti. Vivamus et pulvinar magna. Nam aliquet ipsum elit, vitae ullamcorper dolor facilisis a. Ut non leo et dolor mattis vestibulum eget in sapien. Phasellus aliquam ornare consequat. Vestibulum quis ullamcorper tortor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus cursus turpis eu leo dapibus porta. Vestibulum condimentum dui vitae mauris varius, a dignissim orci porttitor. Etiam sed rhoncus velit. Quisque nec dolor gravida risus venenatis efficitur eu vel augue. Phasellus faucibus arcu at tellus pellentesque ultrices. Nullam quis auctor nunc, a consectetur nunc. Morbi at tincidunt lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+    Phasellus et mi quis felis venenatis fermentum. Pellentesque et rutrum erat, iaculis iaculis ante. Ut consectetur, orci placerat tempus elementum, ligula lectus vulputate est, eget condimentum eros est id tellus. Ut in cursus sem. Cras est dui, pharetra id dui vel, tincidunt mattis arcu. Cras vitae arcu eu mi ornare malesuada. Maecenas vulputate elementum erat nec tempus. Maecenas et velit fringilla, sodales ligula et, ultricies arcu. Aenean eget urna ut eros sollicitudin sollicitudin. Sed ornare pharetra erat, mollis viverra tellus mattis bibendum. Sed tempor nisi at lorem lobortis consectetur. Donec fermentum ac ante in elementum.
+    </body>
+</html>
