c61d3f1f8075280215663382aa97b7ac0b2d52bd
Joshua Slui <joshuaslui0203@gmail.com>
2023-11-22 19:32:07 +0100

    Delete everything and start over

--- src/assets/components/footer.php
+++
@@ -1,3 +0,0 @@
-<footer style="margin-top: 10px">
-    <p>© 2023 - <?php echo date("Y") ?> by Wolfsvale Software<br>Contact: <a href="mailto:support@wolfsvale.tech" style="text-decoration: none;color:var(--white)">support@wolfsvale.tech</a> | KVK: 91173817</p>
-</footer>
