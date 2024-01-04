e05a6fd936dbc2322c85bbc3e7f6e84a58400beb
Joshua Slui <joshuaslui0203@gmail.com>
2024-01-04 19:16:01 +0100

    default index page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Wolfsvale</title>
+		<meta name="og:description" content="Free Open-Source Software, brought to you by Wolfsvale Software, a dutch organisation dedicated to free software and education globally.">
+		<meta name="og:author" content="Wolfsvale Software">
+		<style>@import "assets/css/app.css";</style>
+		<script src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body>
+		<?php include './assets/components/pageParts/navigation.php' ?>
+	</body>
+</html>
