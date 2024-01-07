a7e370066579f0bbf1273ed75298f1b3fa6cfeb8
Joshua Slui <joshuaslui0203@gmail.com>
2024-01-07 18:05:41 +0100

    Add about page

---
+++ about.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>Wolfsvale</title>
+	<meta name="og:description" content="Free Open-Source Software, brought to you by Wolfsvale Software, a dutch organisation dedicated to free software and education globally.">
+	<meta name="og:author" content="Wolfsvale Software">
+	<style>@import "assets/css/app.css";</style>
+	<script src="node_modules/@fortawesome/fontawesome-free/js/all.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php include './assets/components/pageParts/navigation.php' ?>
+
+<section class="hero">
+	<div class="hero__header">
+		<img src>
+	</div>
+		<p>Wolfsvale Software is a reputable Dutch software development organisation aiming to fill the gaps in the FOSS market.</p>
+</section>
+</body>
+</html>
